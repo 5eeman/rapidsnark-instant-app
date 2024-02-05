eb1f24a64f422660341dae0defa4dc17680d4cea
Yaroslav Moria <5eeman@users.noreply.github.com>
2024-02-05 16:12:48 +0100

    Added Android Instant app and iOS App Clip.

---
+++ ios/rapidsnarkAppClip/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  rapidsnarkAppClip
+//
+//  Created by Yaroslav Moria on 31.01.2024.
+//
+
+#import <React/RCTBridgeDelegate.h>
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
+
---
+++ ios/rapidsnarkAppClip/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  rapidsnarkAppClip
+//
+//  Created by Yaroslav Moria on 31.01.2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
